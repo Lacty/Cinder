d474992f1b6bca2fb57fc35440e2fd3f1a277c2c
U-PS14B22\vantan <akira206@gmail.com>
2015-04-19 15:45:40 +0900

    ナゾ

--- Camera/FirstTest.cpp
+++ Camera/FirstTest.cpp
@@ -51,8 +51,8 @@ void CinderProjectApp::setup()
   // 今回はリファレンスと同じ数値にしておく(指定した数値が何になるのかがわからん
   m_cam.setPerspective(60.0f, getWindowAspectRatio(), 5.0f, 3000.0f);
 
-  m_eye    = Vec3f(0.0f, 0.0f, 500.f);
-  m_center = Vec3f::zero();              // Vec3f(0.0f, 0.0f, 0.0f)
+  m_eye    = Vec3f(WIDTH/2, HEIGHT/2, 500.f);
+  m_center = Vec3f(WIDTH/2, HEIGHT/2, 0.0f);              // Vec3f(0.0f, 0.0f, 0.0f)
   m_up     = Vec3f::yAxis();             // Vec3f(0.0f, 1.0f, 0.0f) // Axis(軸)
 
   m_cam.lookAt(m_eye, m_center, m_up);
@@ -74,7 +74,7 @@ void CinderProjectApp::draw()
   gl::color(Color(0, 1, 1)); // 色指定
   { // キューブがクルクル回転
     gl::pushModelView();
-    gl::translate(Vec3f(300, HEIGHT / 2, 0));
+    gl::translate(Vec3f(WIDTH/2, HEIGHT / 2, 0));
     gl::rotate(Vec3f(0.0f, angle, 0.0f));
 
     gl::drawCube(Vec3f(0, 0, 0), size);
@@ -84,8 +84,8 @@ void CinderProjectApp::draw()
 
   gl::setMatrices(m_cam);
 
-  // キューブ中央に描画させてるはずなんだが・・・
-  // 左上で回転してる＾ｐ＾
+  // 目の位置が(0, 0)だったからおかしかった
+  // 目の位置を中央に、centerも中央にもってきたらうまくいった
 }
 
 CINDER_APP_NATIVE(CinderProjectApp, RendererGl)
