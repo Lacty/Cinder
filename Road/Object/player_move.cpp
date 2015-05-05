6207cceba4fa70f44e48f8e85d74db2fe24652d8
U-PS14B22\vantan <akira206@gmail.com>
2015-05-05 12:47:20 +0900

    PlayerMove関係制作

--- Road/Object/player_move.cpp
+++ Road/Object/player_move.cpp
@@ -5,17 +5,33 @@
 
 PlayerMove::PlayerMove(Object* obj) :
 IObject(obj),
-pos(Vec3f::zero()) {}
+pos(Vec3f::zero()),
+lean_status(LeanStatus::Center) {}
 
 
 void PlayerMove::update() {
-  
+  moveLeft();
+  moveRight();
+  moveBound();
 }
 
 void PlayerMove::moveLeft() {
-
+  if (!Key::get().isPress(KeyEvent::KEY_a)) return;
+  if (lean_status != LeanStatus::Left) {
+    lean_status = LeanStatus::Left;
+  }
+  pos.x -= Speed;
 }
 
 void PlayerMove::moveRight() {
+  if (!Key::get().isPress(KeyEvent::KEY_d)) return;
+  if (lean_status != LeanStatus::Right) {
+    lean_status = LeanStatus::Right;
+  }
+  pos.x += Speed;
+}
 
+void PlayerMove::moveBound() {
+  if (pos.x < edge.left)  { pos.x += Speed; }
+  if (pos.x > edge.right) { pos.x -= Speed; }
 }
--- Road/Object/player_move.h
+++ Road/Object/player_move.h
@@ -5,17 +5,24 @@
 
 
 enum class LeanStatus {
+  Center,
   Left,
   Right
 };
 
 class PlayerMove : public IObject {
 private:
+  struct Edge {
+    const float left  = -200.0f;
+    const float right =  200.0f;
+  } edge;
+  const float Speed = 4.0f;
   Vec3f pos;
   LeanStatus lean_status;
 
   void moveLeft();
   void moveRight();
+  void moveBound();
 
 public:
   PlayerMove(Object*);
