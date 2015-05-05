b43c990a00d4b50101ec60832fc2e5b06ffc56ac
U-PS14B22\vantan <akira206@gmail.com>
2015-05-05 12:21:19 +0900

    enum LeanStatus追加

---
+++ Road/Object/player_move.cpp
@@ -0,0 +1,21 @@
+
+#include "player_move.h"
+#include "../MyLib/key.h"
+
+
+PlayerMove::PlayerMove(Object* obj) :
+IObject(obj),
+pos(Vec3f::zero()) {}
+
+
+void PlayerMove::update() {
+  
+}
+
+void PlayerMove::moveLeft() {
+
+}
+
+void PlayerMove::moveRight() {
+
+}
--- Road/Object/player_move.h
+++ Road/Object/player_move.h
@@ -1,14 +1,25 @@
 
 #pragma once
 #include "Iobject.h"
+#include "../MyLib/common.h"
 
+
+enum class LeanStatus {
+  Left,
+  Right
+};
 
 class PlayerMove : public IObject {
 private:
+  Vec3f pos;
+  LeanStatus lean_status;
+
+  void moveLeft();
+  void moveRight();
 
 public:
   PlayerMove(Object*);
 
-  void update();
-  void draw();
+  void  update();
+  Vec3f getPos();
 };
