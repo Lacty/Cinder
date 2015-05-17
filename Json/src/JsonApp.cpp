4e1affc6bce5700fc142a38c38c0090840b71539
U-PS14B22\vantan <akira206@gmail.com>
2015-05-17 21:04:17 +0900

    Json書き込み

--- Json/src/JsonApp.cpp
+++ Json/src/JsonApp.cpp
@@ -9,7 +9,11 @@ using namespace ci::app;
 
 class JsonApp : public AppNative {
 private:
-  float hoge;
+  struct Object {
+    std::string name;
+    float Hp;
+    float speed;
+  } player;
 public:
   void setup();
   void mouseDown(MouseEvent event);
@@ -18,9 +22,19 @@ public:
 };
 
 void JsonApp::setup() {
-  JsonTree json_test(loadAsset("Source.json"));
-  hoge = json_test["object"].getValue<double>();
-  console() << hoge << std::endl;
+  // データ読み込み
+  JsonTree js_load(loadAsset("Source.json"));
+  auto js_player = js_load["Player"];
+  player.name    = js_player["Name"].getValue<std::string>();
+  player.Hp      = js_player["Hp"].getValue<float>();
+  player.speed   = js_player["Speed"].getValue<float>();
+
+  console() << player.name  << std::endl;
+  console() << player.Hp    << std::endl;
+  console() << player.speed << std::endl;
+
+  // データ書き込み
+
 }
 
 void JsonApp::mouseDown(MouseEvent event) {}
